48f359bd2fb2be0463410f6e5abd0eef3389a763
Mariusz "Nightmare" Jarnot <mariusz.jarnot@onet.pl>
2023-10-08 12:58:34 +0200

    PS4 Fedora Experimental Connection / Pair

---
+++ bloothoot.py
@@ -0,0 +1,27 @@
+ 
+import subprocess
+import time
+
+# Set the MAC address of your PS4 controller
+controller_mac_address = '88:03:4C:FF:18:53'  # Replace with your controller's MAC address
+
+def pair_ps4_controller():
+    try:
+        # Put your Bluetooth adapter into pairing mode
+        subprocess.call(['bluetoothctl', 'discoverable', 'on'])
+
+        # Put the controller into pairing mode (hold the Share and PS buttons simultaneously)
+        print('Put the PS4 controller into pairing mode (hold Share + PS buttons)')
+        time.sleep(10)  # You have 10 seconds to put the controller into pairing mode
+
+        # Pair the controller with your computer
+        subprocess.call(['bluetoothctl', 'pair', controller_mac_address])
+        subprocess.call(['bluetoothctl', 'connect', controller_mac_address])
+
+        print('PS4 controller paired successfully!')
+
+    except Exception as e:
+        print(f'Error: {str(e)}')
+
+if __name__ == '__main__':
+    pair_ps4_controller()
