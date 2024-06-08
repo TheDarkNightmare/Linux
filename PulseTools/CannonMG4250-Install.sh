3f14e76f93177077f80a6be010a72f945cc82405
Mariusz "Nightmare" Jarnot <mariusz.jarnot@onet.pl>
2024-06-08 20:00:22 +0200

    Update CannonMG4250-Install.sh

--- PulseTools/CannonMG4250-Install.sh
+++ PulseTools/CannonMG4250-Install.sh
@@ -1,1 +1,17 @@
- 
+#!/bin/bash
+
+echo "[    Cannon MG4250 Printer Drivers Install     ]"
+echo "[ Version 1.01 - TheDarkNightmare - PulseTools ]"
+
+# Update the package database and install CUPS (Common Unix Printing System) and Gutenprint
+sudo pacman -Syu --noconfirm cups gutenprint
+
+# Install the cups-bjnp package for network printing support for Canon printers
+paru -S --noconfirm cups-bjnp
+
+# Enable and start the CUPS service
+sudo systemctl enable --now cups
+
+# Print a message indicating that the installation is complete
+echo "Installation complete. Please add your printer via KDE Printer Settings"
+
