1a61e133dbb471ae7baf9bca5a6912fae656f6e9
TheDarkNightmare <mariusz.jarnot@onet.pl>
2024-06-22 18:31:09 +0200

    test

--- PulseTools/FastFetch-User.sh
+++ PulseTools/FastFetch-User.sh
@@ -1,39 +1,67 @@
- 
 #!/bin/bash
 
 echo "[----------------------------------------------]"
 echo "|                                              |"
 echo "|    FastFetch - System User Configuration     |"
 echo "|                                              |"
-echo "| Version 1.01 - TheDarkNightmare - PulseTools |"
+echo "| Version 1.04 - TheDarkNightmare - PulseTools |"
 echo "|                                              |"
 echo "[----------------------------------------------]"
-
 
 # Update the package database and install CUPS (Common Unix Printing System) and Gutenprint
 sudo pacman -Syu --noconfirm fastfetch
 
+# Generate default config in ~/.config/fastfetch
 fastfetch --gen-config
 
 sleep 5
 
-cd ~/.config/fastfetch/
+# Removing default config for new one
 rm ./config.jsonc
 
-sleep 5
+# Check if the directory exists
+if [ -d "$HOME/KWinScripts/" ]; then
+    cd "$HOME/KWinScripts/"
 
-wget -O config.jsonc https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/config.jsonc
-wget -O ARC2.png https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/ARC2.png
+    sleep 5
 
-sleep 5
-kfontview https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/SymbolsNerdFontMono-Regular.ttf
+    # Downloading Files (config and image logo)
+    wget -O config.jsonc https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/config.jsonc
+    wget -O ARC2.png https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/ARC2.png
 
-sleep 5
-kfontview https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/SymbolsNerdFont-Regular.ttf
+    sleep 5
 
+    # Pulling font icons from repo
+    kfontview https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/SymbolsNerdFontMono-Regular.ttf
 
+    sleep 5
 
+    # Pulling font icons from repo
+    kfontview https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/SymbolsNerdFont-Regular.ttf
 
+    echo "Installation complete. Please check your fastfetch settings"
+else
+    echo "Directory $HOME/KWinScripts/ does not exist."
+    read -p "Would you like to create the directory and download the files? (y/n): " choice
+    if [ "$choice" = "y" ]; then
+        mkdir -p "$HOME/KWinScripts/"
+        cd "$HOME/KWinScripts/"
 
-echo "Installation complete. Please check your fastfetch settings"
+        sleep 5
 
+        # Downloading Files (config and image logo)
+        wget -O config.jsonc https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/config.jsonc
+        wget -O ARC2.png https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/ARC2.png
+
+        sleep 5
+        kfontview https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/SymbolsNerdFontMono-Regular.ttf
+
+        sleep 5
+        kfontview https://raw.githubusercontent.com/TheDarkNightmare/Linux/main/PulseTools/Files/SymbolsNerdFont-Regular.ttf
+
+        echo "Installation complete. Please check your fastfetch settings"
+    else
+        echo "Exiting without making changes."
+        exit 1
+    fi
+fi
