8ec09eee5c1196c03192a355cef8902b89f8b9c5
TheDarkNightmare <mariusz.jarnot@onet.pl>
2024-07-13 04:57:49 +0200

    Patch 1.0.3
    - Replace paru for yay for downloading drivers from AUR.

--- PulseTools/CannonMG4250-Install.sh
+++ PulseTools/CannonMG4250-Install.sh
@@ -4,18 +4,18 @@ echo "[----------------------------------------------]"
 echo "|                                              |"
 echo "|    Cannon MG4250 Printer Drivers Install     |"
 echo "|                                              |"
-echo "| Version 1.01 - TheDarkNightmare - PulseTools |"
+echo "| Version 1.03 - TheDarkNightmare - PulseTools |"
 echo "|                                              |"
 echo "[----------------------------------------------]"
 
-# Check if paru is installed
-if ! command -v paru &> /dev/null
+# Check if yay is installed
+if ! command -v yay &> /dev/null
 then
-    echo "paru is not installed. Installing paru..."
-    # Install paru if not installed - First Base package dev.
+    echo "yay is not installed. Installing yay..."
+    # Install yay if not installed - First Base package dev.
     sudo pacman -S --needed --noconfirm base-devel
-    git clone https://aur.archlinux.org/paru.git
-    cd paru
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
     makepkg -si --noconfirm
     cd ..
     rm -rf paru
@@ -25,7 +25,7 @@ fi
 sudo pacman -Syu --noconfirm cups gutenprint
 
 # Install the cups-bjnp package for network printing support for Canon printers
-paru -S --noconfirm cups-bjnp
+yay -S --noconfirm cups-bjnp
 
 # Enable and start the CUPS service
 sudo systemctl enable --now cups
